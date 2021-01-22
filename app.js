ae08ba3bde5ae89e33a0c698309e93663866a774
Rafael Dona <rafa_dona@hotmail.com>
2021-01-21 21:03:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Comprar comida","Cozinhar","Comer"];
+
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.get("/", function (req, res) {
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+
+let day = today.toLocaleDateString("en-US",options);
+
+    res.render("list", { kindOfDay: day,newListItems: items});
+
+});
+
+app.post("/",function(req,res){
+
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+
+})
+
+
+
+app.listen(3300, function () {
+    console.log('Server started on port 3300');
+});
